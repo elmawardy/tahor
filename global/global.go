49292d1b71ab36c4811c7e79fa8cae51a2d39693
elmawardy <elmawardy@localhost.localdomain>
2020-08-10 08:20:37 +0200

    docker 0

--- global/global.go
+++ global/global.go
@@ -8,13 +8,13 @@ import (
 // DB is global DB Config
 var Config = map[string]string{
 	"driver":    "postgres",
-	"constring": "host=localhost port=5432 user=postgres dbname=tahor password=123456 sslmode=disable",
+	"constring": "host=postgres port=5432 user=postgres dbname=tahor password=123456 sslmode=disable",
 }
 
 var DB *gorm.DB
 
 var Cachestore = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
+	Addr:     "redis:6379",
 	Password: "", // no password set
 	DB:       0,  // use default DB
 })
